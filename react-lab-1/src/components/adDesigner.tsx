7d5b056813612e0319433960ea67399d04419ddd
Ren <rpaczkowski@gmail.com>
2022-07-21 21:36:21 -0400

    Worked on adDesigner

--- react-lab-1/src/components/adDesigner.tsx
+++ react-lab-1/src/components/adDesigner.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./adDesigner.css";
 
 function AdDesigner() {
+  const [buttonValue, setButtonValue] = useState("Rocky Road");
+
+  const handleAdButton = () => {
+    if (buttonValue === "Rocky Road") {
+      setButtonValue("Rocky Road");
+    } else if (buttonValue === "Americone Dream") {
+      setButtonValue("Americone Dream");
+    } else {
+      setButtonValue("Superman");
+    }
+  };
+
   return (
     <div>
-      <h4>Vote for</h4>
-      <h3>{}</h3>
+      <div className="card-container">
+        <h3>Vote for</h3>
+        <h2>{buttonValue}</h2>
+      </div>
+      <div className="button-container">
+        <button onClick={handleAdButton}>Rocky Road</button>
+        <button onClick={handleAdButton}>Americone Dream</button>
+        <button onClick={handleAdButton}>Superman</button>
+      </div>
     </div>
   );
 }
